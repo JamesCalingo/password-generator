1b723d11314bc5945264e5c6adf6766faef2b0bc
JamesCalingo <james.calingo.web@gmail.com>
2022-01-03 21:34:45 +0000

    made python version

--- password.py
+++ password.py
@@ -4,4 +4,11 @@ import string
 def generate_password(length):
   password = ""
   for i in range(length):
-    char = 
+    char = random.choice(string.printable)
+    if char in string.whitespace:
+      char = random.choice(string.printable)
+    else:
+      password += (char)
+  return password
+
+print(generate_password(15))
