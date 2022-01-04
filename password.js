d4751a4e4dafe993ab17c12df56d09b8fc765f0d
JamesCalingo <james.calingo.web@gmail.com>
2022-01-04 18:20:54 +0000

    playing with inputs

--- password.js
+++ password.js
@@ -1,11 +1,16 @@
-const generatePassword = (num) => {
+const generatePassword = (length) => {
+
+  // if(typeof(length) !== Number) {
+  //   console.log("Please use a number for your length!")
+  //   return
+  // }
 let password = ''
 let charStr = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&'
 let addChar = () => {
   const random = Math.floor(Math.random() * charStr.length)
   password += charStr[random]
 }
-for(let i = 0; i < num; i++) {
+for(let i = 0; i < length; i++) {
   addChar()
 if(password[i] === password[i - 1]) {
   password = password.slice(0, -1)
@@ -17,4 +22,4 @@ return password
 
 module.exports = generatePassword
 
-console.log(generatePassword(15))
+console.log(generatePassword())
--- password.py
+++ password.py
@@ -1,14 +1,23 @@
 import random
 import string
 
-def generate_password(length):
+"""This generates a string of randomized characters of a user-defined length"""
+
+def generate_password(length = 15):
+  length = int(input("How long would you like your password to be? "))
+  while length < 8 or length > 20:
+    print("Your password should probably be between 8 and 20 characters. Try again.")
+    length = int(input("How long would you like your password to be? "))
+
   password = ""
   for i in range(length):
     char = random.choice(string.printable)
     if char in string.whitespace:
       char = random.choice(string.printable)
+    # elif char == password[i - 1]:
+    #   char = random.choice(string.printable)
     else:
       password += (char)
   return password
 
-print(generate_password(15))
+print(generate_password())
